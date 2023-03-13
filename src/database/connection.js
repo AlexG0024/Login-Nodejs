64d4157f818fb8cc161116519c412547fcee6c8a
Alex <Naudis0024@gmail.com>
2023-03-13 12:49:30 -0400

    Connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
 const connection = async () => {
-  //const Mongo = process.env.MONGO_URL || "mongodb://localhost:27017"
+  const Mongo = process.env.MONGO_URL || "mongodb://localhost:27017"
   try {
     mongoose.set("strictQuery", false)
-    await mongoose.connect("mongodb+srv://Alex03c4:EOYDdQh90GZQ8Nkf@cluster0.myvgi.mongodb.net/eliminar")
+    await mongoose.connect(Mongo)
     console.log("Conectado correctamente a la base de datos")
   } catch (error) {
     console.log(error)
